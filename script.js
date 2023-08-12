e67039643514f0176b53e086f6e1f5a0ea53d69a
Maruf Bin Murtuza <78444913+marufmurtuza@users.noreply.github.com>
2023-08-12 16:48:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Add the 'animate' class to sections when they are in the viewport
+function animateSectionsInView() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        if (rect.top <= windowHeight * 0.75) {
+            section.classList.add('animate');
+        }
+    });
+}
 
+// Trigger animations on page load and scroll
+window.addEventListener('load', animateSectionsInView);
+window.addEventListener('scroll', animateSectionsInView);
